22b4ce635d1761835e7ea344ac1f7f71c9fd9d36
MochammadQemalFirza <kemalfirza@gmail.com>
2023-08-09 11:57:40 +0700

    task day 3

---
+++ contact.js
@@ -0,0 +1,31 @@
+function submitData() {
+    let name = document.getElementById("input-name").value;
+    let email = document.getElementById("input-email").value;
+    let phone = document.getElementById("input-phone").value;
+    let subject = document.getElementById("input-pilih").value;
+    let message = document.getElementById("input-message").value;
+  
+    if (name == "") {
+      return alert("Nama harus diisi!");
+    } else if (email == "") {
+      return alert("Email harus diisi!");
+    } else if (phone == "") {
+      return alert("Phone harus diisi!");
+    } else if (subject == "") {
+      return alert("Subject harus dipilih!");
+    } else if (message == "") {
+      return alert("Message harus diisi!");
+    }
+  
+  
+    let emailReceiver = "qemalfirza@gmail.com";
+  
+  
+    let a = document.createElement("a");
+    a.href = `mailto:${emailReceiver}?subject=${subject}&body=Halo, nama saya ${name}, ${message}, silakan kontak saya pada nomor ${phone}`;
+    a.click();
+  
+    let data = { name, email, phone, subject, message };
+  
+    console.log(data);
+  }
