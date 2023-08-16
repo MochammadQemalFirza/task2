dee5da2707ca380772f6c7b41f0e0007d34adeda
MochammadQemalFirza <kemalfirza@gmail.com>
2023-08-16 14:26:23 +0700

    task day 7

---
+++ js/testimonials.js
@@ -0,0 +1,120 @@
+class Testimonial {
+  #quote = "";
+  #image = "";
+
+  constructor(quote, image) {
+    this.#quote = quote;
+    this.#image = image;
+  }
+
+  get quote() {
+    return this.#quote;
+  }
+
+  get image() {
+    return this.#image;
+  }
+
+  // This is an abstract method that subclasses will implement
+  get author() {
+    throw new Error("getAuthor() method must be implemented.");
+  }
+
+  // This is a polymorphic method than can take any subclass of Testimonial
+  get testimonialHTML() {
+    return `
+          <div class="testimonial">
+              <img src="${this.image}" class="profile-testimonial" />
+              <p class="quote">
+                  ${this.quote}
+              </p>
+              <p class="author">- ${this.author}</p>
+          </div>
+      `;
+  }
+}
+
+class AuthorTestimonial extends Testimonial {
+  #author = "";
+
+  constructor(author, quote, image) {
+    super(quote, image);
+    this.#author = author;
+  }
+
+  get author() {
+    return this.#author;
+  }
+}
+
+class CompanyTestimonial extends Testimonial {
+  #company = "";
+
+  constructor(author, quote, image) {
+    super(quote, image);
+    this.#company = author;
+  }
+
+  get author() {
+    return this.#company + " Company";
+  }
+}
+
+const testimonial1 = new AuthorTestimonial(
+  "Papah Simpati",
+  "Gelak Tawa",
+  "assets/6e5943034398146eb04e54f37b7a2f5b.jpg"
+);
+
+const testimonial2 = new AuthorTestimonial(
+  "Rajab Gugug",
+  "Gangguin Jawa",
+  "assets/download (2).jpeg"
+);
+
+const testimonial3 = new AuthorTestimonial(
+  "Dimas Kopling",
+  "Ksabar!",
+  "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+);
+
+const testimonial4 = new AuthorTestimonial(
+  "Sawi Putih",
+  "Siapa sangka!",
+  "assets/kanye-west-_150917084801-755.jpg"
+);
+
+const testimonial5 = new AuthorTestimonial(
+  "Abdul Galon",
+  "Terkadang!",
+  "assets/9734c2e6-c84f-447e-94f6-337ca8c9c596-1688175721813.webp"
+);
+
+let testimonialData = [
+  testimonial1,
+  testimonial2,
+  testimonial3,
+  testimonial4,
+  testimonial5,
+];
+let testimonialHTML = "";
+
+for (let i = 0; i < testimonialData.length; i++) {
+  testimonialHTML += testimonialData[i].testimonialHTML;
+}
+
+document.getElementById("testimonials").innerHTML = testimonialHTML;
+
+let hamburgerIsOpen = false;
+const openHamburger = () => {
+  let hamburgerNavContainer = document.getElementById(
+    "hamburger-nav-container"
+  );
+  if (!hamburgerIsOpen) {
+    hamburgerNavContainer.style.display = "block";
+    hamburgerIsOpen = true;
+  } else {
+    hamburgerNavContainer.style.display = "none";
+    hamburgerIsOpen = false;
+  }
+};
