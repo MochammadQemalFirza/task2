0a27f4ea112fb7b131edb673b7f7c95265530186
MochammadQemalFirza <kemalfirza@gmail.com>
2023-08-22 14:21:50 +0700

    task day 9 rev 1

--- js/testimonials_ajax.js
+++ js/testimonials_ajax.js
@@ -1,6 +1,7 @@
+const endpoint = "https://api.npoint.io/62394282cd38263becee";
 const testimonialData = new Promise((resolve, reject) => {
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.npoint.io/62394282cd38263becee", true);
+  xhr.open("GET", endpoint, true);
   xhr.onload = function () {
     if (xhr.status === 200) {
       resolve(JSON.parse(xhr.response));
