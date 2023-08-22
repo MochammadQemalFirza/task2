d8f5d8adfae0bbc6defcf288f1ed2410909d5818
MochammadQemalFirza <kemalfirza@gmail.com>
2023-08-22 13:14:05 +0700

    task day 9

---
+++ js/testimonials_ajax.js
@@ -0,0 +1,72 @@
+const testimonialData = new Promise((resolve, reject) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.npoint.io/62394282cd38263becee", true);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      resolve(JSON.parse(xhr.response));
+    } else {
+      reject("Error loading data");
+    }
+  };
+  xhr.onerror = function () {
+    reject("Network error: " + xhr.status);
+  };
+
+  xhr.send();
+});
+
+async function allTestimonials() {
+  try {
+    const response = await testimonialData;
+    console.log(response);
+    let testimonialHTML = "";
+
+    response.forEach(function (item) {
+      testimonialHTML += `
+              <div class="testimonial">
+                <img src="${item.image}" class="profile-testimonial" />
+                <p class="quote">${item.quote}</p>
+                <p class="author">- ${item.author}</p>
+                <p class="author">${item.rating}<i class="fa-solid fa-star"></i></p>
+              </div>
+              `;
+    });
+
+    document.getElementById("testimonials").innerHTML = testimonialHTML;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+allTestimonials();
+
+async function filterTestimonials(rating) {
+  try {
+    const response = await testimonialData;
+    console.log(response);
+    let testimonialHTML = "";
+
+    const testimonialFiltered = response.filter(function (item) {
+      return item.rating === rating;
+    });
+
+    if (testimonialFiltered.length === 0) {
+      testimonialHTML = `<h1>Data not found!</h1>`;
+    } else {
+      testimonialFiltered.forEach(function (item) {
+        testimonialHTML += `
+            <div class="testimonial">
+              <img src="${item.image}" class="profile-testimonial" />
+              <p class="quote">${item.quote}</p>
+              <p class="author">- ${item.author}</p>
+              <p class="author">${item.rating}<i class="fa-solid fa-star"></i></p>
+            </div>
+            `;
+      });
+    }
+
+    document.getElementById("testimonials").innerHTML = testimonialHTML;
+  } catch (err) {
+    console.log(err);
+  }
+}
